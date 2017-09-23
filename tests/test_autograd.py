a1dabed115d8e3d469593b1ea1e29f5c79c7a64b
Joseph Ilustre <joseph@ilustreous.com>
2017-09-23 06:07:20 -0400

    test

--- tests/test_autograd.py
+++
@@ -1,9 +0,0 @@
-import toolkit.autograd as autograd
-import numba
-
-numba
-
-
-x = autograd.Variable(4)
-
-print(x.id)
--- toolkit/autograd/Variable.py
+++
@@ -1,6 +0,0 @@
-
-
-class Variable(object):
-
-    def __init__(self, variableId):
-        self.id = variableId
